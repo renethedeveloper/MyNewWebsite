26cddd62b16837ff93f241bbad0953d38c5aa1ee
Rene Rodriguez <afuereno@gmail.com>
2023-11-10 22:26:27 -0800

    custom sourcing

--- client/src/Components/CustomSourcing/index.jsx
+++ client/src/Components/CustomSourcing/index.jsx
@@ -1,34 +1,52 @@
-import React from 'react'
-import "./index.css"
+import React, { useState } from 'react';
 
-const CustomSourcing = () => {
+const YourFormComponent = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [priceRange, setPriceRange] = useState(0);
+  const [lookedForItem, setLookedForItem] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePriceChange = (event) => {
+    setPriceRange(event.target.value);
+  };
+
+  const handleLookedForItemChange = (event) => {
+    setLookedForItem(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    // Handle form submission
+    event.preventDefault();
+    // Access the values in the state (name, email, priceRange, lookedForItem)
+    console.log({ name, email, priceRange, lookedForItem });
+  };
+
   return (
-    <div>
-      <div className='mainSourceFormDiv' >
-      <div className='sourceForm'>
-        <form action="">
-          <label for="name">Name</label><br/>
-          <input type="text" id="name" name="name"/><br/>
-          <label for="name">Name</label><br/>
-          <input type="text" id="name" name="name"/><br/>
-          <label for="name">Name</label><br/>
-          <input type="text" id="name" name="name"/><br/>
-          <label for="name">Name</label><br/>
-          <input type="text" id="name" name="name"/><br/>
-          <label for="name">Name</label><br/>
-          <input type="text" id="name" name="name"/><br/>
-          <label for="name">Name</label><br/>
-          <input type="text" id="name" name="name"/><br/>
-         
-        </form>
-       
-      </div>
-      <img className="Flamingos" src="/public/Images/images/Flamingos.jpeg" alt="flamingos" />
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label><br />
+      <input type="text" id="name" name="name" value={name} onChange={handleNameChange} /><br />
 
-      </div>
-      
-    </div>
-  )
-}
+      <label htmlFor="email">Email</label><br />
+      <input type="text" id="email" name="email" value={email} onChange={handleEmailChange} /><br />
 
-export default CustomSourcing
+      <label htmlFor="priceRange">Price Range</label><br />
+      <input type="range" id="priceRange" name="priceRange" min="0" max="1000" step="10" value={priceRange} onChange={handlePriceChange} />
+      <span id="priceValue">{priceRange}</span><br />
+
+      <label htmlFor="lookedForItem">Looked For Item</label><br />
+      <input type="text" id="lookedForItem" name="lookedForItem" value={lookedForItem} onChange={handleLookedForItemChange} /><br />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default YourFormComponent;
--- client/src/Components/Home/index.jsx
+++ client/src/Components/Home/index.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import "./index.css"
+import { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [priceRange, setPriceRange] = useState(0);
+  const [lookedForItem, setLookedForItem] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePriceChange = (event) => {
+    setPriceRange(event.target.value);
+  };
+
+  const handleLookedForItemChange = (event) => {
+    setLookedForItem(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    // Handle form submission
+    event.preventDefault();
+    // Access the values in the state (name, email, priceRange, lookedForItem)
+    console.log({ name, email, priceRange, lookedForItem });
+  };
+
+
+
   return (
     <div>
         
@@ -26,27 +57,27 @@ const Home = () => {
       <div className='sourceForm'>
 
         <h1>Looking for anything in particular? Tell us and we can custom source for you!</h1>
-          
-            <form action="/action_page.php"></form>
         <form action="">
-        <label htmlFor="name">Name</label><br />
-<input type="text" id="name" name="name" /><br />
+      <label htmlFor="name">Name</label><br />
+      <input type="text" id="name" name="name" /><br />
 
-<label htmlFor="email">Email</label><br />
-<input type="text" id="email" name="email" /><br />
+      <label htmlFor="email">Email</label><br />
+      <input type="text" id="email" name="email" /><br />
 
-<label htmlFor="priceRange">Price Range</label><br />
-<input type="range" id="priceRange" name="priceRange" min="0" max="1000" step="10" /><br />
- 
+      <label htmlFor="priceRange">Price Range</label><br />
+      <input className='priceSlide' type="range" id="priceRange" name="priceRange" min="0" max="1000" step="10" value={priceRange} onChange={handlePriceChange} />
+      <span id="priceValue">${priceRange}</span><br />
 
-<label htmlFor="lookedForItem">Looked For Item</label><br />
-<input type="text" id="lookedForItem" name="lookedForItem" /><br />
+      <label htmlFor="lookedForItem">Desired Item</label><br />
+      <input type="text" id="lookedForItem" name="lookedForItem" /><br />
 
-         
-        </form>
+      <button type="submit">Submit</button>
+    </form>
+
+
        
       </div>
-      <img className="Flamingos" src="/public/Images/images/Flamingos.jpeg" alt="flamingos" />
+      <img className="flamingos" src="/public/Images/images/Flamingos.jpeg" alt="flamingos" />
 
       </div>
       
