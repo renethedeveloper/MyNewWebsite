e7a86baac16e82414b9ff8b3fcedf0b00c6ea4ba
Rene Rodriguez <afuereno@gmail.com>
2023-11-11 11:03:43 -0800

    pukey cats

--- client/src/Components/Home/index.jsx
+++ client/src/Components/Home/index.jsx
@@ -1,8 +1,5 @@
-import React from 'react'
-import "./index.css"
-import { useState } from 'react';
-
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import "./index.css";
 
 const Home = () => {
   const [name, setName] = useState('');
@@ -26,65 +23,53 @@ const Home = () => {
     setLookedForItem(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    // Handle form submission
-    event.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
     // Access the values in the state (name, email, priceRange, lookedForItem)
     console.log({ name, email, priceRange, lookedForItem });
+    alert(`Thank you ${name}! We have received your information.`);
   };
-
-
 
   return (
     <div>
-        
-        <img className='mainImage' src="Images/images/showRoomMain.png" alt="showroom" />
+      <img className='mainImage' src="Images/images/showRoomMain.png" alt="showroom" />
+      <img className='map' src="Images/images/mapToShop.png" alt="map" />
+      <p className='directions'>Under Drip Tea Market!!</p>
 
-        <img className='map' src="Images/images/mapToShop.png" alt="map" />
-        <p className='directions'>Under Drip Tea Market!!</p>
-        
-        
-       
-        <div className='gallery'>
+      <div className='gallery'>
         <img id='galleryImage' src="Images/images/flamingoMirror.jpeg" alt="flamingoMirror" />
         <img id='galleryImage' src="Images/images/tulipLampsGold.jpeg" alt="tulipLamp" />
         <img id='galleryImage' src="Images/images/leatherChair.jpeg" alt="leatherChair" />
+      </div>
+
+      <div className='mainSourceFormDiv'>
+        <h1 className='sourcingTitle'>Looking for anything in particular? Tell us and we can custom source for you!</h1>
+        <div className='customSourcing'>
+          <div className='sourceForm'>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="name">Name</label><br />
+              <input type="text" id="name" name="name" /><br />
+
+              <label htmlFor="email">Email</label><br />
+              <input type="text" id="email" name="email" /><br />
+
+              <label htmlFor="priceRange">Price Range</label><br />
+              <input className='priceSlide' type="range" id="priceRange" name="priceRange" min="0" max="1000" step="10" value={priceRange} onChange={handlePriceChange} />
+              <span id="priceValue">${priceRange}</span><br />
+
+              <label htmlFor="lookedForItem">Describe the desired item. The more detail, the better.</label><br />
+              <textarea className="desiredItem" id="lookedForItem" name="lookedForItem"></textarea>
+
+              <button type="submit" style={{ backgroundColor: '#4caf50', color: 'white', padding: '10px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Submit</button>
+            </form>
+          </div>
+          <div>
+            <img className="flamingos" src="/public/Images/images/Flamingos.jpeg" alt="flamingos" />
+          </div>
         </div>
-        
-
-        <div>
-      <div className='mainSourceFormDiv' >
-      <div className='sourceForm'>
-
-        <h1>Looking for anything in particular? Tell us and we can custom source for you!</h1>
-        <form action="">
-      <label htmlFor="name">Name</label><br />
-      <input type="text" id="name" name="name" /><br />
-
-      <label htmlFor="email">Email</label><br />
-      <input type="text" id="email" name="email" /><br />
-
-      <label htmlFor="priceRange">Price Range</label><br />
-      <input className='priceSlide' type="range" id="priceRange" name="priceRange" min="0" max="1000" step="10" value={priceRange} onChange={handlePriceChange} />
-      <span id="priceValue">${priceRange}</span><br />
-
-      <label htmlFor="lookedForItem">Desired Item</label><br />
-      <input type="text" id="lookedForItem" name="lookedForItem" /><br />
-
-      <button type="submit">Submit</button>
-    </form>
-
-
-       
       </div>
-      <img className="flamingos" src="/public/Images/images/Flamingos.jpeg" alt="flamingos" />
-
-      </div>
-      
     </div>
-      
-    </div>
-  )
-}
+  );
+};
 
 export default Home;
