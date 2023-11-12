3c829f6ec7e22cf8cf13169449b54668c27c6469
Rene Rodriguez <afuereno@gmail.com>
2023-11-12 15:43:04 -0800

    email wont' work

--- backend/server.js
+++ backend/server.js
@@ -1,83 +1,117 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
-const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
 const Product = require("./models/index.jsx");
-require('dotenv').config();
+require("dotenv").config();
 require("./config");
-const path = require("path")
+const path = require("path");
+const transporter = require('./config/transporter.js');
 
 const PORT = 3000;
-
 const app = express();
 
 // Middleware
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "dist")));
 
 
-app.use((req, res, next)=> {
-    if (req.path.startsWith('/server')) {
-        req.url = req.url.replace('/server', ''); // strip /server from the path
+
+
+app.post("/submitForm", async (req, res) => {
+    try {
+      const { name, email, priceRange, lookedForItem } = req.body;
+  
+      const mailOptions = {
+        from: email,
+        to: "afuereno@gmail.com",
+        subject: "New Form Submission",
+        text: `
+          Name: ${name}
+          Email: ${email}
+          Price Range: ${priceRange}
+          Looked For Item: ${lookedForItem}
+        `,
+      };
+  
+      const info = await transporter.sendMail(mailOptions);
+      console.log("Email sent", info.response);
+      res.status(200).send("Form submitted successfully!");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
     }
-    next();
   });
+  
 
-// Routes
-// app.get("/", async (req, res)=>{
-//     res.send("Hello and welcome.")
-// })
 
+
+
+
+
+
+
+app.use((req, res, next) => {
+  if (req.path.startsWith("/server")) {
+    req.url = req.url.replace("/server", ""); // strip /server from the path
+  }
+  next();
+});
 
 app.post("/products", async (req, res) => {
-    try {
-        const newProduct = req.body;
-        if (!newProduct.title || !newProduct.description || !newProduct.price) {
-            res.status(400).send("Bad Request: Product name and price are required.");
-        } else {
-            const product = await Product.create(newProduct);
-            console.log(`Created product: ${product}`);
-            res.status(201).send(product);
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Internal Server Error: Failed to create the product.");
+  try {
+    const newProduct = req.body;
+    if (!newProduct.title || !newProduct.description || !newProduct.price) {
+      res.status(400).send("Bad Request: Product name and price are required.");
+    } else {
+      const product = await Product.create(newProduct);
+      console.log(`Created product: ${product}`);
+      res.status(201).send(product);
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error: Failed to create the product.");
+  }
 });
 
 app.get("/products", async (req, res) => {
-    try {
-        const productArray = await Product.find();
-        res.json(productArray);
-        console.log(productArray)
-    } catch (error) {
-        console.error("Error fetching products:", error);
-        res.status(500).send("An error occurred while fetching products");
-    }
+  try {
+    const productArray = await Product.find();
+    res.json(productArray);
+    console.log(productArray);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).send("An error occurred while fetching products");
+  }
 });
 
 app.get("/products/:id", async (req, res) => {
-    const id = req.params.id;
-    try {
-        const product = await Product.findById(id);
-        if (!product) {
-            res.status(404).send("Product not found");
-        } else {
-            res.json(product);
-        }
-    } catch (error) {
-        console.error("Error fetching product:", error);
-        res.status(500).send("An error occurred while fetching the product");
+  const id = req.params.id;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.status(404).send("Product not found");
+    } else {
+      res.json(product);
     }
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    res.status(500).send("An error occurred while fetching the product");
+  }
 });
 
-// The code should continue running your server without the MongoDB connection part here.
+app.use((err, req, res, next) => {
+    console.error("Error:", err);
+    res.status(500).send("Internal Server Error");
+  });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
--- client/src/Components/Home/index.jsx
+++ client/src/Components/Home/index.jsx
@@ -1,80 +1,183 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./index.css";
+import axios from "axios";
 
 const Home = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [priceRange, setPriceRange] = useState(0);
-  const [lookedForItem, setLookedForItem] = useState('');
+  const [sourcingData, setSourcingData] = useState({
+    name: "",
+    email: "",
+    priceRange: 0,
+    lookedForItem: "",
+  });
+
+  const { name, email, priceRange, lookedForItem } = sourcingData;
 
   const handleNameChange = (event) => {
-    setName(event.target.value);
+    setSourcingData({ ...sourcingData, name: event.target.value });
   };
 
   const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+    setSourcingData({ ...sourcingData, email: event.target.value });
   };
 
   const handlePriceChange = (event) => {
-    setPriceRange(event.target.value);
+    setSourcingData({ ...sourcingData, priceRange: event.target.value });
   };
 
   const handleLookedForItemChange = (event) => {
-    setLookedForItem(event.target.value);
+    setSourcingData({ ...sourcingData, lookedForItem: event.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Access the values in the state (name, email, priceRange, lookedForItem)
-    console.log({ name, email, priceRange, lookedForItem });
-    alert(`Thank you ${name}! We have received your information.`);
+    console.log(sourcingData)
+    axios({
+      method: "post",
+      url: "/server/submitForm",
+      data: sourcingData,
+    })
+      .then((response) => {
+        console.log(response);
+        console.log("we got this far.");
+
+        alert(
+          `Thank you ${sourcingData.name}! We have received your information.`
+        );
+
+        setSourcingData({
+          name: "",
+          email: "",
+          priceRange: 0,
+          lookedForItem: "",
+        });
+      })
+      .catch((error) => {
+        console.error("Error submitting form:", error);
+        alert("Oops! Something went wrong. Please try again later.");
+      });
   };
 
   return (
     <div>
-  <img className='mainImage' src="Images/images/showRoomMain.png" alt="showroom" />
-  <img className='map' src="Images/images/mapToShop.png" alt="map" />
-  <p className='directions'>Under Drip Tea Market!!</p>
+      <img
+        className="mainImage"
+        src="Images/images/showRoomMain.png"
+        alt="showroom"
+      />
+      <img className="map" src="Images/images/mapToShop.png" alt="map" />
+      <p className="directions">Under Drip Tea Market!!</p>
 
-  <div className='gallery-container'>
-    <div className='gallery-item'>
-      <img className='galleryImage' src="Images/images/flamingoMirror.jpeg" alt="flamingoMirror" />
-      <h2 className='title'>Decor</h2>
-    </div>
+      <div className="gallery-container">
+        <div className="gallery-item">
+          <img
+            className="galleryImage"
+            src="Images/images/flamingoMirror.jpeg"
+            alt="flamingoMirror"
+          />
+          <h2 className="title">Decor</h2>
+        </div>
 
-    <div className='gallery-item'>
-      <img className='galleryImage' src="Images/images/tulipLampsGold.jpeg" alt="tulipLamp" />
-      <h2 className='title'>Lighting</h2>
-    </div>
+        <div className="gallery-item">
+          <img
+            className="galleryImage"
+            src="Images/images/tulipLampsGold.jpeg"
+            alt="tulipLamp"
+          />
+          <h2 className="title">Lighting</h2>
+        </div>
 
-    <div className='gallery-item'>
-      <img className='galleryImage' src="Images/images/leatherChair.jpeg" alt="leatherChair" />
-      <h2 className='title'>Furniture</h2>
-    </div>
-  </div>
-      <div className='mainSourceFormDiv'>
-        <h1 className='sourcingTitle'>Looking for anything in particular? Tell us and we can custom source for you!</h1>
-        <div className='customSourcing'>
-          <div className='sourceForm'>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="name">Name</label><br />
-              <input type="text" id="name" name="name" /><br />
+        <div className="gallery-item">
+          <img
+            className="galleryImage"
+            src="Images/images/leatherChair.jpeg"
+            alt="leatherChair"
+          />
+          <h2 className="title">Furniture</h2>
+        </div>
+      </div>
+      <div className="mainSourceFormDiv">
+        <h1 className="sourcingTitle">
+          Looking for anything in particular? Tell us and we can custom source
+          for you!
+        </h1>
+        <div className="customSourcing">
+          <div className="sourceForm">
+            <form
+              action="http://localhost:3000/submitForm"
+              method="post"
+              onSubmit={handleSubmit}
+            >
+              <label htmlFor="name">Name</label>
+              <br />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => handleNameChange(e)}
+              />
+              <br />
 
-              <label htmlFor="email">Email</label><br />
-              <input type="text" id="email" name="email" /><br />
+              <label htmlFor="email">Email</label>
+              <br />
+              <input
+                type="text"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => handleEmailChange(e)}
+              />
+              <br />
 
-              <label htmlFor="priceRange">Price Range</label><br />
-              <input className='priceSlide' type="range" id="priceRange" name="priceRange" min="0" max="1000" step="10" value={priceRange} onChange={handlePriceChange} />
-              <span id="priceValue">${priceRange}</span><br />
+              <label htmlFor="priceRange">Price Range</label>
+              <br />
+              <input
+                className="priceSlide"
+                type="range"
+                id="priceRange"
+                name="priceRange"
+                min="0"
+                max="1000"
+                step="10"
+                value={priceRange}
+                onChange={(e) => handlePriceChange(e)}
+              />
+              <span id="priceValue">${priceRange}</span>
+              <br />
 
-              <label htmlFor="lookedForItem">Describe the desired item. The more detail, the better.</label><br />
-              <textarea className="desiredItem" id="lookedForItem" name="lookedForItem"></textarea>
+              <label htmlFor="lookedForItem">
+                Describe the desired item. The more detail, the better.
+              </label>
+              <br />
+              <textarea
+                className="desiredItem"
+                id="lookedForItem"
+                name="lookedForItem"
+                value={lookedForItem}
+                onChange={(e) => handleLookedForItemChange(e)}
+              ></textarea>
 
-              <button type="submit" style={{ backgroundColor: '#4caf50', color: 'white', padding: '10px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Submit</button>
+              <button
+                type="submit"
+                style={{
+                  backgroundColor: "#4caf50",
+                  color: "white",
+                  padding: "10px",
+                  border: "none",
+                  borderRadius: "4px",
+                  cursor: "pointer",
+                }}
+              >
+                Submit
+              </button>
             </form>
           </div>
           <div>
-            <img className="flamingos" src="/public/Images/images/Flamingos.jpeg" alt="flamingos" />
+            <img
+              className="flamingos"
+              src="/public/Images/images/Flamingos.jpeg"
+              alt="flamingos"
+            />
           </div>
         </div>
       </div>
